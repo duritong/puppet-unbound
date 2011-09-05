923e42f89f0571792636e9ac2e1f181ab4ab90c2
mh <mh@immerda.ch>
2011-09-05 22:26:34 +0200

    enable source/ensure options on includes and actually activate them

--- manifests/conf.pp
+++ manifests/conf.pp
@@ -1,7 +1,26 @@
-class unbound::conf($content){
+class unbound::conf(
+  $ensure = present,
+  $content = 'absent',
+  $source = 'absent'
+){
   file{"/etc/unbound/conf.d/${name}.conf":
-    content => $content,
+    ensure => $ensure,
     notify => Service['unbound'],
     owner => root, group => 0, mode => 0644;
+  }
+  if $source != 'absent' {
+    File["/etc/unbound/conf.d/${name}.conf"]{
+      source => $source
+    }
+  } else {
+    File["/etc/unbound/conf.d/${name}.conf"]{
+      content => $content,
+    }
+  }
+
+  line{"${name}_unbound_include":
+    line => "Include: /etc/unbound/conf.d/${name}.conf",
+    file => "/etc/unbound/conf.d/includes.conf",
+    ensure => $ensure,
   }
 }
