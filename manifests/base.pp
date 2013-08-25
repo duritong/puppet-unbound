7dfd7163c790f3f06ac84ea4c9cb477bbfee60f5
mh <mh@immerda.ch>
2013-08-25 23:20:14 +0200

    make it work on debian (at least wheezy

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,25 +1,31 @@
+# setup all necessary stuff for unbound
 class unbound::base {
   package{'unbound':
     ensure => present,
   }
 
+  $unbound_interface = $unbound::interface ? {
+    ''      => '',
+    default => "interface: ${unbound::interface}\n"
+  }
+
   file{
     '/etc/unbound/unbound.conf':
-      source => "puppet:///modules/unbound/config/unbound.conf";
+      source  => [
+        "puppet:///modules/unbound/config/unbound.conf.${::operatingsystem}",
+        'puppet:///modules/unbound/config/unbound.conf',
+      ];
     '/etc/unbound/conf.d':
-      ensure => directory,
-      purge => true,
-      force => true,
+      ensure  => directory,
+      purge   => true,
+      force   => true,
       recurse => true;
     '/etc/unbound/conf.d/includes.conf':
-      ensure => present;
+      ensure  => present;
     '/etc/unbound/conf.d/server_acls.conf':
       content => template('unbound/server_acls.conf.erb');
     '/etc/unbound/conf.d/server_interface.conf':
-      content => $unbound::interface ? {
-        '' => '',
-        default => "interface: ${unbound::interface}\n"
-      };
+      content => $unbound_interface;
     '/etc/unbound/conf.d/local_data.conf':
       source => [ "puppet:///modules/site_unbound/${::fqdn}/config/conf.d/local_data.conf",
                   "puppet:///modules/site_unbound/${::domain}/config/conf.d/local_data.conf",
@@ -27,17 +33,19 @@ class unbound::base {
                   'puppet:///modules/unbound/config/conf.d/local_data.conf' ];
   }
   File['/etc/unbound/unbound.conf','/etc/unbound/conf.d',
-       '/etc/unbound/conf.d/includes.conf',
-       '/etc/unbound/conf.d/server_acls.conf',
-       '/etc/unbound/conf.d/local_data.conf',
-       '/etc/unbound/conf.d/server_interface.conf']{
+        '/etc/unbound/conf.d/includes.conf',
+        '/etc/unbound/conf.d/server_acls.conf',
+        '/etc/unbound/conf.d/local_data.conf',
+        '/etc/unbound/conf.d/server_interface.conf']{
     require => Package['unbound'],
-    notify => Service['unbound'],
-    owner => root, group => 0, mode => 0644
+    notify  => Service['unbound'],
+    owner   => root,
+    group   => 0,
+    mode    => 0644
   }
 
   service{'unbound':
-    enable => true,
     ensure => running,
+    enable => true,
   }
 }
