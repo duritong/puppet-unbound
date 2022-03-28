209f20f26a8aa66d21b4e0e3014ddb0ddca8578a
mh <mh@immerda.ch>
2022-03-28 12:19:41 +0200

    filter out empty values

--- manifests/base.pp
+++ manifests/base.pp
@@ -6,7 +6,7 @@ class unbound::base {
 
   $unbound_interface_str = $unbound::interface ? {
     'all'   => '',
-    default => "${Array($unbound::interface,true).map |$i| { "interface: ${i}" }.join("\n")}\n",
+    default => "${Array($unbound::interface,true).filter |$i| { $i !~ String[1] }.map |$i| { "interface: ${i}" }.join("\n")}\n",
   }
 
   file {
